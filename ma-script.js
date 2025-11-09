d207115b575c1d035b289cdfbc28d0e44759260b
saver-grand <savergrand6@gmail.com>
2025-11-09 10:04:41 +0800

    Update ma-script.js

--- ma-script.js
+++ ma-script.js
@@ -360,7 +360,7 @@ const channels = [
     title: "🏀 NBA- Indiana Pacers vs. Denver Nuggets",
     date: "2025-11-09",
     time: "10:00 AM",
-    server1: "",
+    server1: "https://e22cd317d72ce9c3afea3614629c9397.livehwc4.com/nami.videobss.com/live/hd-en-2-3866743.m3u8?sub_m3u8=true&edge_slice=true&user_session_id=5b4c7fa0ba2ff85d311765d12bec5f80",
     server2: "https://streamcenter.pro/embed/ch71.php"
   },
   {
