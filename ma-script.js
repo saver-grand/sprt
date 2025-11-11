88a25034a7f5dbf25e8d138f954e7b5ddd45ccc4
saver-grand <savergrand6@gmail.com>
2025-11-11 10:03:58 +0800

    Update ma-script.js

--- ma-script.js
+++ ma-script.js
@@ -359,7 +359,7 @@ const channels = [
     title: "🏀 NBA - New Orleans Pelicans vs. Phoenix Suns",
     date: "2025-11-11",
     time: "10:00 AM",
-    server1: "https://gg.poocloud.in/nba_phoenixsuns/index.m3u8",
+    server1: "https://e22cd317d72ce9c3afea3614629c9397.livehwc4.com/nami.videobss.com/live/hd-en-2-3867054.m3u8?sub_m3u8=true&edge_slice=true&user_session_id=9ea9b9563929b3780df6b36bcd31ce33",
     server2: "https://streamcenter.pro/embed/ch71.php"
   },
   {
