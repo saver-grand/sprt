c268229c726d78fb76e2940ae2ed4a875a12abba
saver-grand <savergrand6@gmail.com>
2025-11-15 10:41:57 +0800

    Update ma-script.js

--- ma-script.js
+++ ma-script.js
@@ -383,7 +383,7 @@ const channels = [
     title: "🏀 NBA - Golden State Warriors vs. San Antonio Spurs",
     date: "2025-11-15",
     time: "10:30 AM",
-    server1: "https://gg.poocloud.in/nba_sanantoniospurs/index.m3u8",
+    server1: "https://e22cd317d72ce9c3afea3614629c9397.livehwc4.com/nami.videobss.com/live/hd-en-2-3866894.m3u8?sub_m3u8=true&edge_slice=true&user_session_id=fd13cbd4f10aca88afc3122164904e8f",
     server2: "https://streamcenter.pro/embed/ch73.php"
   }
 ];
