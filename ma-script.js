b5dc6ffd62eeea1f963dcd1861a344fcc06258a6
saver-grand <savergrand6@gmail.com>
2025-11-15 09:34:20 +0800

    Update ma-script.js

--- ma-script.js
+++ ma-script.js
@@ -375,7 +375,7 @@ const channels = [
     title: "🏀 NBA - LA Clippers vs. Dallas Mavericks",
     date: "2025-11-15",
     time: "9:30 AM",
-    server1: "https://gg.poocloud.in/nba_dallasmavericks/index.m3u8",
+    server1: "https://e22cd317d72ce9c3afea3614629c9397.livehwc4.com/nami.videobss.com/live/hd-en-2-3867003.m3u8?sub_m3u8=true&edge_slice=true&user_session_id=079956923bd4e75acf834623da5e3b7e",
     server2: "https://streamcenter.pro/embed/ch72.php"
   },
   {
