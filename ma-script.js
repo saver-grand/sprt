42d88cb2ac02a78021366036a82b859ebf12fc01
saver-grand <savergrand6@gmail.com>
2025-11-15 08:34:34 +0800

    Update ma-script.js

--- ma-script.js
+++ ma-script.js
@@ -335,7 +335,7 @@ const channels = [
     title: "🏀 NBA - Philadelphia 76ers vs. Detroit Pistons",
     date: "2025-11-15",
     time: "8:30 AM",
-    server1: "https://gg.poocloud.in/nba_detroitpistons/index.m3u8",
+    server1: "https://e22cd317d72ce9c3afea3614629c9397.livehwc4.com/nami.videobss.com/live/hd-en-2-3866248.m3u8?sub_m3u8=true&edge_slice=true&user_session_id=78164899643d43fa1969ef2b54b835bb",
     server2: "https://streamcenter.pro/embed/ch67.php"
   },
   {
