8d5cf94c6ae288f90795e6dcc7efcac9dd737b14
saver-grand <savergrand6@gmail.com>
2025-11-17 06:56:24 +0800

    Update ma-script.js

--- ma-script.js
+++ ma-script.js
@@ -327,7 +327,7 @@ const channels = [
   title: "🏀 NBA - Sacramento Kings vs. San Antonio Spurs",
   date: "2025-11-17",
   time: "5:00 AM",
-  server1: "",
+  server1: "https://e22cd317d72ce9c3afea3614629c9397.livehwc4.com/nami.videobss.com/live/hd-en-2-3866957.m3u8?sub_m3u8=true&edge_slice=true&user_session_id=184216dbaf5db12794814d93cda87123",
   server2: "https://streamcenter.pro/embed/ch66.php"
 },
 {
