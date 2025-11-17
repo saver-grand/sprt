fdae5c7016435da533d0601f5e4bb51a66aeec04
saver-grand <savergrand6@gmail.com>
2025-11-17 08:04:05 +0800

    Update ma-script.js

--- ma-script.js
+++ ma-script.js
@@ -343,7 +343,7 @@ const channels = [
   title: "🏀 NBA - Orlando Magic vs. Houston Rockets",
   date: "2025-11-17",
   time: "8:00 AM",
-  server1: "",
+  server1: "https://e22cd317d72ce9c3afea3614629c9397.livehwc4.com/nami.videobss.com/live/sd-2-3866072.m3u8?sub_m3u8=true&edge_slice=true&user_session_id=da7a66d5f7748f9cb73e14efdc36c73e",
   server2: "https://streamcenter.pro/embed/ch68.php"
 },
 {
@@ -351,7 +351,7 @@ const channels = [
   title: "🏀 NBA - Golden State Warriors vs. New Orleans Pelicans",
   date: "2025-11-17",
   time: "8:00 AM",
-  server1: "",
+  server1: "https://e22cd317d72ce9c3afea3614629c9397.livehwc4.com/nami.videobss.com/live/sd-2-3867067.m3u8?sub_m3u8=true&edge_slice=true&user_session_id=ff64e8cbb59cf1fafd818f2d1560995f",
   server2: "https://streamcenter.pro/embed/ch69.php"
 },
 {
