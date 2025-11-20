247fd896f6fdd5ccee14848d655b3ff8e55fa0b6
saver-grand <savergrand6@gmail.com>
2025-11-20 11:09:04 +0800

    Update ma-script.js

--- ma-script.js
+++ ma-script.js
@@ -375,7 +375,7 @@ const channels = [
     title: "🏀 NBA - New York Knicks vs. Dallas Mavericks",
     date: "2025-11-20",
     time: "10:30 AM",
-    server1: "https://nba.honotvph.42web.io/nba8/index.m3u8",
+    server1: "https://e22cd317d72ce9c3afea3614629c9397.livehwc4.com/nami.videobss.com/live/hd-en-2-3866671.m3u8?sub_m3u8=true&edge_slice=true&user_session_id=a9ef49766367f4c406b631e04b13f79d",
     server2: "https://streamcenter.xyz/embed/ch72.php"
   },
   {
