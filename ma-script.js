beaf351f4c20a61a90ef34845d343ec513c97c6a
saver-grand <savergrand6@gmail.com>
2025-11-22 08:06:09 +0800

    Update ma-script.js

--- ma-script.js
+++ ma-script.js
@@ -327,7 +327,7 @@ const channels = [
     title: "🏀 NBA - Indiana Pacers vs. Cleveland Cavaliers",
     date: "2025-11-22",
     time: "8:00 AM",
-    server1: "https://gg.poocloud.in/nba_clevelandcavaliers/index.m3u8",
+    server1: "https://e22cd317d72ce9c3afea3614629c9397.livehwc4.com/nami.videobss.com/live/hd-en-2-3867021.m3u8?sub_m3u8=true&edge_slice=true&user_session_id=fbb4b50518abc0f6bff1bdf7882fa32d",
     server2: "https://streamcenter.xyz/embed/ch65.php"
 },
 {
